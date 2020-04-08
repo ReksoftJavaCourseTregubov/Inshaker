b6ba71660c03c659e539e9b78df74e6c25bef84f
MailRuUnityCourseTregubov <compmath2017@gmail.com>
2020-04-08 21:21:36 +0300

    Add application config

---
+++ src/main/java/ru/vsu/amm/inshaker/configuration/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package ru.vsu.amm.inshaker.configuration;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public Mapper mapper() {
+        return new DozerBeanMapper();
+    }
+
+}
