1abd188cc6d579ed73a875b30b32dbcd3f6e9d2c
MailRuUnityCourseTregubov <compmath2017@gmail.com>
2020-05-05 16:21:16 +0300

    Update security config

--- src/main/java/ru/vsu/amm/inshaker/model/dto/maps/CocktailAmountDTO.java
+++ src/main/java/ru/vsu/amm/inshaker/model/dto/maps/CocktailAmountDTO.java
@@ -1,22 +1,18 @@
 package ru.vsu.amm.inshaker.model.dto.maps;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.vsu.amm.inshaker.model.dto.simple.CocktailSimpleDTO;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CocktailAmountDTO {
 
     @JsonIgnoreProperties("recipe")
     private CocktailSimpleDTO cocktail;
     private Short amount;
-
-    public CocktailAmountDTO() {
-    }
-
-    public CocktailAmountDTO(CocktailSimpleDTO cocktail, Short amount) {
-        this.cocktail = cocktail;
-        this.amount = amount;
-    }
 
 }
