1a28b8a67de7bc9d2eb5099bdec24b65fe7c99e0
MailRuUnityCourseTregubov <compmath2017@gmail.com>
2020-05-20 17:39:21 +0300

    Restore group and category in properties dto

--- src/main/java/ru/vsu/amm/inshaker/dto/properties/IngredientPropertiesDTO.java
+++ src/main/java/ru/vsu/amm/inshaker/dto/properties/IngredientPropertiesDTO.java
@@ -3,9 +3,7 @@ package ru.vsu.amm.inshaker.dto.properties;
 import lombok.Data;
 import ru.vsu.amm.inshaker.model.Taste;
 import ru.vsu.amm.inshaker.model.enums.Spirit;
-import ru.vsu.amm.inshaker.model.item.properties.Country;
-import ru.vsu.amm.inshaker.model.item.properties.IngredientBase;
-import ru.vsu.amm.inshaker.model.item.properties.ItemSubgroup;
+import ru.vsu.amm.inshaker.model.item.properties.*;
 
 import java.util.List;
 
@@ -13,6 +11,10 @@ import java.util.List;
 public class IngredientPropertiesDTO {
 
     private List<ItemSubgroup> subGroups;
+
+    private List<ItemGroup> groups;
+
+    private List<ItemCategory> categories;
 
     private List<Taste> tastes;
 
