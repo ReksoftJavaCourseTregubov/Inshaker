39a5699adb08ad736fe9846bd99cfb95f7fcf54d
MailRuUnityCourseTregubov <compmath2017@gmail.com>
2020-06-01 17:19:48 +0300

    Remove unnecessary methods override in GarnishController

--- src/main/java/ru/vsu/amm/inshaker/controllers/items/GarnishController.java
+++ src/main/java/ru/vsu/amm/inshaker/controllers/items/GarnishController.java
@@ -1,13 +1,10 @@
 package ru.vsu.amm.inshaker.controllers.items;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.vsu.amm.inshaker.dto.entire.items.GarnishDTO;
 import ru.vsu.amm.inshaker.model.item.Garnish;
 import ru.vsu.amm.inshaker.services.items.GarnishService;
-
-import javax.annotation.security.RolesAllowed;
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/garnish")
@@ -15,26 +12,6 @@ public class GarnishController extends IngredientController<Garnish, GarnishDTO>
 
     public GarnishController(GarnishService garnishService) {
         super(garnishService);
-    }
-
-    @Override
-    @GetMapping("/{id}")
-    public ResponseEntity<GarnishDTO> one(@PathVariable Long id) {
-        return super.one(id);
-    }
-
-    @Override
-    @RolesAllowed("ROLE_ADMIN")
-    @PostMapping
-    public ResponseEntity<GarnishDTO> add(@RequestBody @Valid GarnishDTO garnish) {
-        return super.add(garnish);
-    }
-
-    @Override
-    @RolesAllowed("ROLE_ADMIN")
-    @PutMapping("/{id}")
-    public ResponseEntity<GarnishDTO> update(@PathVariable Long id, @RequestBody @Valid GarnishDTO garnish) {
-        return super.update(id, garnish);
     }
 
 }
